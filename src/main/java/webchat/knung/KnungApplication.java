8ae639cebbff768e2a700c300010bac848a8ea9a
Hoon1999 <kjhoon44@gmail.com>
2024-03-15 16:02:11 +0900

    first commit

---
+++ src/main/java/webchat/knung/KnungApplication.java
@@ -0,0 +1,13 @@
+package webchat.knung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/webchat/knung/KnungApplicationTests.java
@@ -0,0 +1,13 @@
+package webchat.knung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
