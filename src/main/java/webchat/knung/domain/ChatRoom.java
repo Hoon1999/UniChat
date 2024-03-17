1afca62c28b17bf0d7b074eb7dd50d436fb2aed6
Hoon1999 <kjhoon44@gmail.com>
2024-03-17 17:16:31 +0900

    add: Entity related to chatting

---
+++ src/main/java/webchat/knung/domain/ChatRoom.java
@@ -0,0 +1,18 @@
+package webchat.knung.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "chatRoom")
+public class ChatRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "chat_id", unique = true, nullable = false)
+    long id;
+    @Column(name = "name", nullable = false)
+    String name;
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    Member member;
+}
---
+++ src/main/java/webchat/knung/domain/ParticipationChatRoom.java
@@ -0,0 +1,27 @@
+package webchat.knung.domain;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "participationChatRoom")
+public class ParticipationChatRoom {
+    @Id
+    @Column(name = "id", unique = true, nullable = false)
+    long id;
+    @Column(name = "chat_id", unique = true, nullable = false)
+    long chatId;
+    @Column(name = "chat_seq", nullable = false)
+    long chatSeq;
+    @CreationTimestamp
+    @UpdateTimestamp
+    @Column(name = "last_date", nullable = false)
+    LocalDateTime lastDate;
+
+    @ManyToOne
+    @JoinColumn(name = "chat_id")
+    ChatRoom chatRoom;
+}
