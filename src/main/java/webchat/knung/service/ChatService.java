1512d9b15470006d7d38b31886e714e247dae548
Hoon1999 <kjhoon44@gmail.com>
2024-04-08 12:11:41 +0900

    add: Load Chatting Room list 채팅방을 조회하여 클라이언트에게 반환하는 코드 작성

--- src/main/java/webchat/knung/service/ChatService.java
+++ src/main/java/webchat/knung/service/ChatService.java
@@ -10,6 +10,15 @@ public class ChatService {
     public void listSearch(Member member) {
         member 에서 id를 꺼냄;
         꺼낸 id로 db에 내가 속한 채팅방 조회;
+    ParticipationChatRoomRepository participationChatRoomRepository;
+    MessageRepository messageRepository;
+
+    public ChatService(ParticipationChatRoomRepository participationChatRoomRepository, MessageRepository messageRepository) {
+        this.participationChatRoomRepository = participationChatRoomRepository;
+        this.messageRepository = messageRepository;
     }
 
+    public List<ChatRoomDto> listSearch(Member member) {
+        return participationChatRoomRepository.findByMemberId(member.getMemberId());
+    }
 }
