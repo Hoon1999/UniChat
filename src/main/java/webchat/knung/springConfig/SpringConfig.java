483a4b6312bf8f1e493404db8d78787b9ae7ad99
Hoon1999 <kjhoon44@gmail.com>
2024-04-08 12:41:30 +0900

    modify: config

--- src/main/java/webchat/knung/springConfig/SpringConfig.java
+++ src/main/java/webchat/knung/springConfig/SpringConfig.java
@@ -5,18 +5,29 @@ import org.springframework.context.annotation.Configuration;
 import webchat.knung.controller.LoginController;
 import webchat.knung.domain.Member;
 import webchat.knung.repository.MemberRepository;
+import webchat.knung.repository.MessageRepository;
+import webchat.knung.repository.ParticipationChatRoomRepository;
+import webchat.knung.service.ChatService;
 import webchat.knung.service.MemberService;
 
 @Configuration
 public class SpringConfig {
     private final MemberRepository memberRepository;
+    private final ParticipationChatRoomRepository participationChatRoomRepository;
+    private final MessageRepository messageRepository;
 
-    public SpringConfig(MemberRepository memberRepository) {
+    public SpringConfig(MemberRepository memberRepository, ParticipationChatRoomRepository participationChatRoomRepository, MessageRepository messageRepository) {
         this.memberRepository = memberRepository;
+        this.participationChatRoomRepository = participationChatRoomRepository;
+        this.messageRepository = messageRepository;
     }
 
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository);
+    }
+    @Bean
+    public ChatService chatService() {
+        return new ChatService(participationChatRoomRepository, messageRepository);
     }
 }
