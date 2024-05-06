aff5e5515381b39a34d0bd3d2a188e0fbe349506
Hoon1999 <kjhoon44@gmail.com>
2024-05-07 03:58:58 +0900

    modify: message mapping and send to 클라이언트가 n번 방에 메세지를 발행할 수 있게 동적으로 Message Mapping 되도록 수정함.

--- src/main/java/webchat/knung/controller/StompController.java
+++ src/main/java/webchat/knung/controller/StompController.java
@@ -1,5 +1,6 @@
 package webchat.knung.controller;
 
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -7,11 +8,12 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class StompController {
     // 일단 테스트용 코드
-    @MessageMapping("/room") // 클라이언트가 /chatting/room 으로 메세지를 보내면 이 메서드에서 캐치한다.
-    @SendTo("/topic/message") // send to 는 메서드의 리턴 값이 향하는 곳을 설정한다.(리턴 값을 받을 메세지 브로커를 설정한다.)
-    public String myStompHandler1(String message) {
+    @MessageMapping("/room/{roomId}") // 클라이언트가 /chatting/room/방번호 로 메세지를 보내면 이 메서드에서 캐치한다.
+    @SendTo("/topic/room/{roomId}") // send to 는 메서드의 리턴 값이 향하는 곳을 설정한다.(리턴 값을 받을 메세지 브로커를 설정한다.)
+    public String myStompHandler1(@DestinationVariable("roomId") String roomId, String message) {
         System.out.println("클라이언트가 보낸 메세지 : " + message);
 
         return message;
     }
+
 }
