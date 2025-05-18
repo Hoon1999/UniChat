f644596bd3209de604423eafe0a8df8f0d778009
hoon1999 <kjhoon44@gmail.com>
2025-05-18 10:33:20 +0900

    refactor: 로그인 기능 수정 form submit 방식에서 ajax 로 변경 로그인 실패 메시지 추가 로그인 하지않고 다음 페이지로 넘어가면 로그인 화면으로 이동하도록 수정 메서드 이름 오타 수정

--- src/main/java/webchat/unichat/controller/LoginController.java
+++ src/main/java/webchat/unichat/controller/LoginController.java
@@ -6,11 +6,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import webchat.unichat.domain.Member;
 import webchat.unichat.form.LoginForm;
 import webchat.unichat.form.MemberForm;
 import webchat.unichat.service.MemberService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class LoginController {
@@ -30,9 +34,13 @@ public class LoginController {
     public String login2() {
         return "login";
     }
-    // 로그인
-    @PostMapping("/athentication")
-    public String athentication(LoginForm form, HttpSession session, Model model) {
+
+    // 로그인 인증
+    @PostMapping("/authentication")
+    @ResponseBody
+    public Map<String, String> authentication(@RequestBody LoginForm form, HttpSession session) {
+        Map<String, String> response = new HashMap<>();
+        System.out.println(form.toString());
         Member member = new Member();
         member.setLoginId(form.getLoginId());
         member.setPassword(form.getPassword());
@@ -42,13 +50,25 @@ public class LoginController {
             // 로그인 성공. 메인 페이지로 이동
             session.setAttribute("memberId", member.getMemberId());
             session.setMaxInactiveInterval(1800); // 30분간 세션 유지
-            model.addAttribute("memberId", member.getMemberId());
-            return "chatting_room_list";
+//            response.put("memberId", member.getMemberId().toString());
+            response.put("result", "success");
+            response.put("message", "/chatting_room_list");
         }
         else {
             // 로그인 실패. 로그인 페이지로 이동
+            response.put("result", "fail");
+            response.put("message", "로그인 실패\n 아이디 또는 비밀번호를 확인해주세요" );
+        }
+        return response;
+    }
+    @GetMapping("/chatting_room_list")
+    public String chattingRoomList(HttpSession session) {
+        if(session.getAttribute("memberId") == null) {
+            // 로그인하지 않고 주소창에 /chatting_room_list 를 입력하고 넘어오면
+            // 로그인 창으로 돌려보낸다.
             return "login";
         }
+        return "chatting_room_list";
     }
 
     // 회원가입
